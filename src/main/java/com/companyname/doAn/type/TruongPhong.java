4013919d176fa0de499e7b85fbfa97022aea1473
thanhhieu3004 <thanhhieunguyen3004@gmail.com>
2024-11-30 14:13:10 +0700

    Hieu

--- src/main/java/com/companyname/doAn/type/TruongPhong.java
+++ src/main/java/com/companyname/doAn/type/TruongPhong.java
@@ -7,18 +7,15 @@ public class TruongPhong extends NhanSu {
     private  final String chucVu = "Truong Phong";
     private final int luongCoBan = 4000000;
     private final int soNgayNghiToiDa = 3;
-    PhongBan phongBan;
 
     public TruongPhong() {}
 
-    public TruongPhong(String id, String name, String phone, String diaChi, int namVaoLam, int soNgayNghi, KyLuat[] dsKyLuat, KhenThuong[] dsKhenThuong, int kinhNghiem, PhongBan pb) {
+    public TruongPhong(String id, String name, String phone, String diaChi, int namVaoLam, int soNgayNghi, KyLuat[] dsKyLuat, KhenThuong[] dsKhenThuong, int kinhNghiem) {
         super(id, name, phone, diaChi, namVaoLam, soNgayNghi, dsKyLuat, dsKhenThuong, kinhNghiem);
-        this.phongBan = pb;
     }
 
-    public TruongPhong(String id, String name, String phone, String diaChi, int namVaolam, int kinhNghiem, PhongBan pb) {
+    public TruongPhong(String id, String name, String phone, String diaChi, int namVaolam, int kinhNghiem) {
         super(id, name, phone, diaChi, namVaolam, kinhNghiem);
-        this.phongBan = pb;
     }
 
     @Override
@@ -70,14 +67,6 @@ public class TruongPhong extends NhanSu {
         return luongCoBan + getLuongMem();
     }
 
-    public PhongBan getPhongBan(){
-        return this.phongBan;
-    }
-
-    public void setPhongBan(PhongBan pb){
-        this.phongBan = pb;
-    }
-
     public void printThongTinCoBan(){
         System.out.println("Tên: " + super.getName());
         System.out.println("ID: " + super.getId());
@@ -86,6 +75,5 @@ public class TruongPhong extends NhanSu {
         System.out.println("Năm vào làm: " + super.getNamVaoLam());
         System.out.println("Kinh nghiệm: " + super.getKinhNghiem());
         System.out.println("Lương tháng: " + this.tinhLuong());
-        System.out.printf("Quản lý phòng ban: " + this.phongBan.getNamePhongBan() + ". ID phòng ban: " + this.phongBan.getIdPhongBan());
     }
 }
